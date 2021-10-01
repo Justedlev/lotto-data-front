fd10712302f7d9cca2f02facadaf911d8ddf7e61
Edward {•...•} <59051661+Justedlev@users.noreply.github.com>
2021-10-01 17:22:26 +0300

    Update: actions

--- src/store/actions/repeatable-actions.ts
+++ src/store/actions/repeatable-actions.ts
@@ -4,20 +4,19 @@ import { Repeatable } from "../../models/Repeatable";
 import ReturnTypes from "../../models/ReturnTypes";
 import { convertDateToString } from "../../utils/converter";
 import { setLoadingRepeatableAction } from "./loading-actions";
-import { setReturnCodeAction } from "./return-code-actions";
+import { returnCodeAction } from "./return-code-actions";
 
 export const SET_REPEATABLES_DATA = "set-repeatables-data";
 export const SET_REPEATABLES = "set-repeatables";
 export const SET_REPEATABLES_RECEIVED = "set-repeatables-received";
+export const SET_REPEATABLES_MESSAGE = "set-repeatables-message";
 export const SET_REPEATABLE_NUMBER = "set-repeatable-number";
 export const SET_REPEATABLE_COUNT = "set-repeatable-count";
 export const SET_REPEATABLE_FROM_DATE = "set-repeatable-from-date";
 export const SET_REPEATABLE_TO_DATE = "set-repeatable-to-date";
 export const SET_REPEATABLE_ALLORSTRONG = "set-repeatable-allorstrong";
 
-export function setRepeatablesAction(
-    repeatables: ReturnTypes[]
-): (dispatch: any) => void {
+export function setRepeatablesAction(repeatables: ReturnTypes[]): (dispatch: any) => void {
     return (dispatch) => {
         dispatch({ type: SET_REPEATABLES, payload: repeatables });
     };
@@ -33,48 +32,58 @@ export function getRepeatablesAction(
             dispatch(setLoadingRepeatableAction(true));
             dispatch({
                 type: SET_REPEATABLES_DATA,
-                payload: { isReceived: true, repeatables: [] },
+                payload: { message: "", isReceived: false, repeatables: [] },
             });
             const fromDate: string = convertDateToString(from);
             const toDate: string = convertDateToString(to);
-            const repeatables: Repeatable[] =
-                await service.getTicketsRepeatableNumbersOfRangeDate(
-                    fromDate,
-                    toDate,
-                    allOrStrong
-                );
+            const repeatables: Repeatable[] = await service.getTicketsRepeatableNumbersOfRangeDate(
+                fromDate,
+                toDate,
+                allOrStrong
+            );
             dispatch({
                 type: SET_REPEATABLES_DATA,
-                payload: { isReceived: true, repeatables },
+                payload: {
+                    message:
+                        repeatables.length === 0
+                            ? `По запросу: ${allOrStrong} с ${convertDateToString(
+                                  from
+                              )} до ${convertDateToString(to)} ничего не найдено!`
+                            : "Успешно получено!",
+                    isReceived: true,
+                    data: repeatables,
+                },
             });
-            dispatch(setReturnCodeAction(ReturnTypes.OK));
+            dispatch(returnCodeAction(ReturnTypes.OK));
             dispatch(setLoadingRepeatableAction(false));
         } catch (e) {
-            dispatch(setReturnCodeAction(ReturnTypes.NETWORK_ERROR));
+            dispatch({
+                type: SET_REPEATABLES_DATA,
+                payload: {
+                    message: "Произошла ошибка",
+                    isReceived: false,
+                    data: [],
+                },
+            });
+            dispatch(returnCodeAction(ReturnTypes.NETWORK_ERROR));
             dispatch(setLoadingRepeatableAction(false));
         }
     };
 }
 
-export function setRepeatableFromDateAction(
-    fromDate: Date
-): (dispatch: any) => void {
+export function setRepeatableFromDateAction(fromDate: Date): (dispatch: any) => void {
     return (dispatch) => {
         dispatch({ type: SET_REPEATABLE_FROM_DATE, payload: fromDate });
     };
 }
 
-export function setRepeatableToDateAction(
-    toDate: Date
-): (dispatch: any) => void {
+export function setRepeatableToDateAction(toDate: Date): (dispatch: any) => void {
     return (dispatch) => {
         dispatch({ type: SET_REPEATABLE_TO_DATE, payload: toDate });
     };
 }
 
-export function setRepeatableAllOrStrongAction(
-    allOrStrong: string
-): (dispatch: any) => void {
+export function setRepeatableAllOrStrongAction(allOrStrong: string): (dispatch: any) => void {
     return (dispatch) => {
         dispatch({ type: SET_REPEATABLE_ALLORSTRONG, payload: allOrStrong });
     };
--- src/store/actions/tickets-actions.ts
+++ src/store/actions/tickets-actions.ts
@@ -1,120 +1,45 @@
 import service from "../../config/lotto-data-config";
 import ReturnTypes from "../../models/ReturnTypes";
-import { DEFAULT, SUCCESSFUL, Ticket, UNSUCCESSFUL } from "../../models/Ticket";
-import { setReturnCodeAction } from "./return-code-actions";
-import {
-    setLoadingAddTicketAction,
-    setLoadingDeleteTicketAction,
-    setLoadingTicketsAction,
-} from "./loading-actions";
+import { Ticket } from "../../models/Ticket";
+import { returnCodeAction } from "./return-code-actions";
+import { setLoadingDeleteTicketAction, setLoadingTicketsAction } from "./loading-actions";
 
-export const SET_GET_TICKETS = "set-get-tickets";
-export const SET_ID_TICKET = "set-id-ticket";
-export const SET_TICKET = "set-ticket";
-export const SET_EDIT_TICKET = "set-edit-ticket";
-export const SET_DATE_OF_TICKET = "set-date-of-ticket";
-export const SET_COMBINATION_SIX_NUMBERS_OF_TICKET = "set-combination-six-numbers-of-ticket";
-export const SET_COMBINATION_STRONG_NUMBER_OF_TICKET = "set-combination-strong-number-of-ticket";
-export const SET_SAVED = "set-saved";
-export const SET_RECEIVED = "set-received";
-export const SET_DELETED = "set-deleted";
-
-export function setDeletedTicketAction(isdeleted: boolean): (dispatch: any) => void {
-    return (dispatch) => {
-        dispatch({ type: SET_DELETED, payload: isdeleted });
-    };
-}
+export const SET_TICKET_LIST_DATA = "set-tickets";
+export const SET_TICKET_LIST_DATA_RECEIVED = "set-tickets-received";
+export const SET_TICKET_LIST_DATA_MESSAGE = "set-tickets-message";
+export const SET_TICKET_LIST_DATA_DELETE_TICKET = "set-delete-ticket-from-tickets";
+export const SET_TICKET_LIST_DATA_EDIT_TICKET = "set-delete-ticket-from-tickets";
 
 export function setReceivedTicketsAction(isReceived: boolean): (dispatch: any) => void {
     return (dispatch) => {
-        dispatch({ type: SET_RECEIVED, payload: isReceived });
+        dispatch({ type: SET_TICKET_LIST_DATA_RECEIVED, payload: isReceived });
     };
 }
 
-export function setSavedTicketAction(msg: string): (dispatch: any) => void {
+export function setDeleteTicketAction(isDeleted: boolean): (dispatch: any) => void {
     return (dispatch) => {
-        dispatch({ type: SET_SAVED, payload: msg });
-    };
-}
-
-export function setTicketAction(ticket: Ticket): (dispatch: any) => void {
-    return (dispatch) => {
-        dispatch({ type: SET_TICKET, payload: ticket });
-    };
-}
-
-export function addIdTicketAction(id: number): (dispatch: any) => void {
-    return (dispatch) => {
-        dispatch({ type: SET_ID_TICKET, payload: id });
-        dispatch(setSavedTicketAction(DEFAULT));
-    };
-}
-
-export function addDateOfTicketAction(date: Date): (dispatch: any) => void {
-    return (dispatch) => {
-        dispatch({ type: SET_DATE_OF_TICKET, payload: date });
-        dispatch(setSavedTicketAction(DEFAULT));
-    };
-}
-
-export function addCombinationSixNumbersOfTicketAction(sixNumbers: number[]): (dispatch: any) => void {
-    return (dispatch) => {
-        dispatch({
-            type: SET_COMBINATION_SIX_NUMBERS_OF_TICKET,
-            payload: sixNumbers,
-        });
-        dispatch(setSavedTicketAction(DEFAULT));
-    };
-}
-
-export function addCombinationStrongNumberOfTicketAction(strong: number): (dispatch: any) => void {
-    return (dispatch) => {
-        dispatch({
-            type: SET_COMBINATION_STRONG_NUMBER_OF_TICKET,
-            payload: strong,
-        });
-        dispatch(setSavedTicketAction(DEFAULT));
+        dispatch({ type: SET_TICKET_LIST_DATA_DELETE_TICKET, payload: isDeleted });
     };
 }
 
 export function editTicketAction(ticket: Ticket): (dispatch: any) => void {
     return (dispatch) => {
-        dispatch({ type: SET_EDIT_TICKET, payload: ticket });
+        dispatch({ type: SET_TICKET_LIST_DATA_EDIT_TICKET, payload: ticket });
     };
 }
 
-export function getTicketsAction(): (dispatch: any) => void {
+export function ticketsAction(): (dispatch: any) => void {
     return async (dispatch) => {
         try {
             dispatch(setLoadingTicketsAction(true));
             const tickets: Ticket[] = await service.getTickets();
-            dispatch(setLoadingTicketsAction(false));
-            dispatch({ type: SET_GET_TICKETS, payload: tickets });
+            dispatch({ type: SET_TICKET_LIST_DATA, payload: tickets });
             dispatch(setReceivedTicketsAction(tickets != null));
-            dispatch(setReturnCodeAction(ReturnTypes.OK));
-        } catch (e) {
+            dispatch(returnCodeAction(ReturnTypes.OK));
             dispatch(setLoadingTicketsAction(false));
-            dispatch(setReturnCodeAction(ReturnTypes.NETWORK_ERROR));
-        }
-    };
-}
-
-export function saveTicketAction(ticket: Ticket): (dispatch: any) => void {
-    return async (dispatch) => {
-        try {
-            dispatch(setLoadingAddTicketAction(true));
-            const t: Ticket = await service.addTicket(ticket);
-            dispatch(setLoadingAddTicketAction(false));
-            dispatch(addIdTicketAction(NaN));
-            dispatch(addDateOfTicketAction(new Date()));
-            dispatch(addCombinationSixNumbersOfTicketAction(new Array(6).fill(NaN)));
-            dispatch(addCombinationStrongNumberOfTicketAction(NaN));
-            dispatch(setSavedTicketAction(t != null ? SUCCESSFUL : UNSUCCESSFUL));
-            dispatch(setReturnCodeAction(ReturnTypes.OK));
         } catch (e) {
-            dispatch(setLoadingAddTicketAction(false));
-            dispatch(setSavedTicketAction(UNSUCCESSFUL));
-            dispatch(setReturnCodeAction(ReturnTypes.NETWORK_ERROR));
+            dispatch(returnCodeAction(ReturnTypes.NETWORK_ERROR));
+            dispatch(setLoadingTicketsAction(false));
         }
     };
 }
@@ -125,12 +50,12 @@ export function deleteTicketAction(numberOfTicket: number): (dispatch: any) => v
             dispatch(setLoadingDeleteTicketAction(true));
             const t: Ticket = await service.deteleTicket(numberOfTicket);
             console.log(t);
+            dispatch(ticketsAction());
+            dispatch(returnCodeAction(ReturnTypes.OK));
             dispatch(setLoadingDeleteTicketAction(false));
-            dispatch(getTicketsAction());
-            dispatch(setReturnCodeAction(ReturnTypes.OK));
         } catch (e) {
+            dispatch(returnCodeAction(ReturnTypes.NETWORK_ERROR));
             dispatch(setLoadingDeleteTicketAction(false));
-            dispatch(setReturnCodeAction(ReturnTypes.NETWORK_ERROR));
         }
     };
 }
